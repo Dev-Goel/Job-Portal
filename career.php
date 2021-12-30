29ad7263bba1e6984e9370b932931fd162e528e2
Dev Goel <75497964+Dev-Goel@users.noreply.github.com>
2021-12-30 12:12:49 +0530

    Modified career.php

--- career.php
+++ career.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
+    <!-- <style>
         .navbar {
             overflow: hidden;
             background-color: #333;
@@ -16,13 +16,16 @@
             width: 100%;
             /* Full width */
         }
-    </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </style> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <title>Career</title>
 </head>
 
 <body>
-    <div class="container-fluid">
+    <!-- <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
@@ -47,14 +50,20 @@
                 </div>
             </div>
         </nav>
-        <div>
-            <img src="banner.jpeg" alt="" class="img-fluid" height="50%">
+    </div> -->
+
+    <div class="row">
+        <div class="col-12">
+            <div class="jumbotron jumbotron-fluid" style="background-image: url('banner.jpeg'); background-size: cover;">
+                <div class="container">
+                    <h1 class="display-4" style="color: white;">Job Portal</h1>
+                    <p class="lead" style="color: white;">Best Jobs available matching your skills.</p>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
 </body>
 
 </html>
