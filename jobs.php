6a72f0ddeaf7278dc083064a10ba9dafb27f11a8
Dev Goel <75497964+Dev-Goel@users.noreply.github.com>
2021-12-30 12:13:17 +0530

    Create jobs.php

---
+++ jobs.php
@@ -0,0 +1,34 @@
+<?php include 'header.php' ?>
+
+<div class="content">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Candidate Name</th>
+                <th scope="col">Designation</th>
+                <th scope="col">Resume</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Dev Goel</td>
+                <td>Software Engineer</td>
+                <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Ayaan Goel</td>
+                <td>Software Development Engineer</td>
+                <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ritesh Goel</td>
+                <td>Management Trainee</td>
+                <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
