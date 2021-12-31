66bbc8d55f2fe05c650aaa12f224f9228393925a
Dev Goel <75497964+Dev-Goel@users.noreply.github.com>
2021-12-31 15:52:40 +0530

    Made Candidates Applied (Jobs.php) page dynamic

--- jobs.php
+++ jobs.php
@@ -1,3 +1,4 @@
+<?php include 'config.php'; ?>
 <?php include 'header.php' ?>
 
 <div class="content">
@@ -6,29 +7,49 @@
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Candidate Name</th>
-                <th scope="col">Designation</th>
+                <th scope="col">Applied for</th>
+                <th scope="col">Qualification</th>
+                <th scope="col">Passout Year</th>
                 <th scope="col">Resume</th>
             </tr>
         </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Dev Goel</td>
-                <td>Software Engineer</td>
-                <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
-            </tr>
-            <tr>
+
+        <?php
+        $sql = "SELECT `name`, `apply`, `qual`, `year` FROM `candidates`";
+        $result = mysqli_query($conn, $sql);
+        $i = 0;
+        if ($result->num_rows > 0) {
+            while ($rows = $result->fetch_assoc()) {
+                echo '
+                    <tbody>
+                    <tr>
+                        <th scope="row">' . ++$i . '</th>
+                        <td>' . $rows['name'] . '</td>
+                        <td>' . $rows['apply'] . '</td>
+                        <td>' . $rows['qual'] . '</td>
+                        <td>' . $rows['year'] . '</td>
+                        <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
+                    </tr>';
+            }
+        } else {
+            echo "";
+        }
+
+        ?>
+        <!-- <tr>
                 <th scope="row">2</th>
                 <td>Ayaan Goel</td>
+                <td>Amazon</td>
                 <td>Software Development Engineer</td>
                 <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
             </tr>
             <tr>
                 <th scope="row">3</th>
                 <td>Ritesh Goel</td>
+                <td>ICICI Bank</td>
                 <td>Management Trainee</td>
                 <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
-            </tr>
+            </tr> -->
         </tbody>
     </table>
 </div>
