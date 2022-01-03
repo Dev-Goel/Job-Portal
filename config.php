b335be7978ae4c5016f89b081527e4a79fbe00af
Dev Goel <75497964+Dev-Goel@users.noreply.github.com>
2022-01-03 10:46:22 +0530

    corrected indent

--- config.php
+++ config.php
@@ -1,73 +1,66 @@
 <?php
 
-$server='localhost';
-$username='root';
-$password='';
-$database='jobs';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'jobs';
 
-$conn= mysqli_connect($server, $username, $password, $database);
+$conn = mysqli_connect($server, $username, $password, $database);
 
-if($conn->connect_error){
-    die("Connection failed:".$conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed:" . $conn->connect_error);
 }
-echo"";
+echo "";
 
-if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $number=$_POST['phone_no'];
-    $password=$_POST['password'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['phone_no'];
+    $password = $_POST['password'];
 
     $sql = "INSERT INTO `users`(`Name`, `email`, `password`, `phone_no`) VALUES ('$name' ,'$email','$password','$number')";
-    if(mysqli_query($conn, $sql)){
+    if (mysqli_query($conn, $sql)) {
         echo "Records inserted sucessfully.";
-    }
-    else
-    {
-        echo "ERROR: Could not able to execute $sql. " .mysqli_error($conn);
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
     }
 }
 
 session_start();
-if(isset($_POST['Login'])){
+if (isset($_POST['Login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $query="SELECT * FROM users WHERE `email` = '$email' AND `password` = '$password' ";
+    $query = "SELECT * FROM users WHERE `email` = '$email' AND `password` = '$password' ";
     $result = mysqli_query($conn, $query);
-    $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
-    if(mysqli_num_rows($result)==1)
-    {
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    if (mysqli_num_rows($result) == 1) {
         header("location:index.php");
-    }
-    else
-    {
-        $error='emailid or password is incorrect';
+    } else {
+        $error = 'emailid or password is incorrect';
     }
 }
 
-if(isset($_POST['job'])){
-    $cname=$_POST['cname'];
-    $pos=$_POST['pos'];
-    $Jdesc=$_POST['Jdesc'];
-    $skills=$_POST['skills'];
-    $CTC=$_POST['CTC'];
+if (isset($_POST['job'])) {
+    $cname = $_POST['cname'];
+    $pos = $_POST['pos'];
+    $Jdesc = $_POST['Jdesc'];
+    $skills = $_POST['skills'];
+    $CTC = $_POST['CTC'];
 
     $sql = "INSERT INTO `jobs`(`cname`, `position`, `Jdesc`, `skills`, `CTC`) VALUES ('$cname','$pos','$Jdesc','$skills','$CTC')";
-    if(mysqli_query($conn,$sql)){
+    if (mysqli_query($conn, $sql)) {
         echo "New Job Posted Successfully!!";
-    }
-    else
-    {
-        echo "ERROR: Failed to Post the Job $sql. " .mysqli_error($conn);
+    } else {
+        echo "ERROR: Failed to Post the Job $sql. " . mysqli_error($conn);
     }
 }
 
-if(isset($_POST['submitapply'])){
-    $name=$_POST['name'];
-    $apply=$_POST['apply'];
-    $qual=$_POST['qual'];   
-    $year=$_POST['year'];
+if (isset($_POST['submitapply'])) {
+    $name = $_POST['name'];
+    $apply = $_POST['apply'];
+    $qual = $_POST['qual'];
+    $year = $_POST['year'];
 
     $sql = "INSERT INTO `candidates`(`name`, `apply`, `qual`, `year`) VALUES ('$name','$apply','$qual','$year')";
 
@@ -75,12 +68,10 @@ if(isset($_POST['submitapply'])){
     // var_dump($sql);
     // die();
 
-    if(mysqli_query($conn,$sql)){
+    if (mysqli_query($conn, $sql)) {
         echo "Applied for job successfully!!";
-    }
-    else
-    {
-        echo "ERROR: Failed to apply for the job $sql. " .mysqli_error($conn);
+    } else {
+        echo "ERROR: Failed to apply for the job $sql. " . mysqli_error($conn);
     }
 }
 //mysqli_close($conn);
